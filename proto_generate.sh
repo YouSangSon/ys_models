f1f5647e88d8ffcc374ad375c6e703c971de93ec
yss911001 <yss911001@gmail.com>
2024-09-03 00:16:02 +0900

    * 2nd

--- proto_generate.sh
+++ proto_generate.sh
@@ -11,11 +11,9 @@ protoc -I=protos --python_out=./python ${PROTO_FILE}
 
 # Generate Kotlin code (using Java code generation)
 protoc -I=protos --java_out=./java --kotlin_out=./kotlin ${PROTO_FILE}
+# protoc -I=protos --java_out=./java --grpc-java_out=./java ${PROTO_FILE}
 
 # Generate Go code with go_package option
 protoc -I=protos --go_out=./golang --go_opt=paths=source_relative --go-grpc_out=./golang --go-grpc_opt=paths=source_relative ${PROTO_FILE}
 
-# Generate JavaScript code
-protoc -I=protos --js_out=import_style=commonjs,binary:./javascript ${PROTO_FILE}
-
-echo "Code generation completed for Python, Java, Kotlin, Golang, and JavaScript."
+echo "Code generation completed for Python, Java, Kotlin, Golang."
