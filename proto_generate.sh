ba2af0c1da01949d6e6a2035c1e6f3fa8c1016cd
ysson <ys.son@retigrid.com>
2024-09-02 18:34:20 +0900

    * first

--- proto_generate.sh
+++ proto_generate.sh
@@ -10,7 +10,7 @@ protoc -I=protos --python_out=./python ${PROTO_FILE}
 #protoc -I=protos --java_out=./java protos/users.proto
 
 # Generate Kotlin code (using Java code generation)
-protoc -I=protos --kotlin_out=./kotlin ${PROTO_FILE}
+protoc -I=protos --java_out=./java --kotlin_out=./kotlin ${PROTO_FILE}
 
 # Generate Go code with go_package option
 protoc -I=protos --go_out=./golang --go_opt=paths=source_relative --go-grpc_out=./golang --go-grpc_opt=paths=source_relative ${PROTO_FILE}
