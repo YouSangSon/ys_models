69d27b60f777aa7bac8a0a077a185fd7745d7df2
ysson <ys.son@retigrid.com>
2024-09-03 11:31:39 +0900

    * user

--- proto_generate.sh
+++ proto_generate.sh
@@ -6,14 +6,11 @@ PROTO_FILE=protos/users.proto
 # Generate Python code
 protoc -I=protos --python_out=./python ${PROTO_FILE}
 
-# Generate Java code
-#protoc -I=protos --java_out=./java protos/users.proto
-
 # Generate Kotlin code (using Java code generation)
 protoc -I=protos --java_out=./java --kotlin_out=./kotlin ${PROTO_FILE}
-# protoc -I=protos --java_out=./java --grpc-java_out=./java ${PROTO_FILE}
 
 # Generate Go code with go_package option
 protoc -I=protos --go_out=./golang --go_opt=paths=source_relative --go-grpc_out=./golang --go-grpc_opt=paths=source_relative ${PROTO_FILE}
+#protoc --go_out=. --go_opt=paths=source_relative users.proto
 
 echo "Code generation completed for Python, Java, Kotlin, Golang."
